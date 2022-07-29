cc04be9d92556cfa0b3c2aa1f1df1b85eda7c52b
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-07-29 17:13:01 +0300

    changes to components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Box } from '@mui/material'
+import { Box } from '@mui/material';
 
 import './App.css';
 import ExerciseDetail from './pages/ExerciseDetail';
@@ -8,72 +8,16 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-const App = () => {
-    const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobileNumber, setMobileNumber] = useState("");
-  const [message, setMessage] = useState("");
-
-  let handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      let res = await fetch("https://httpbin.org/post", {
-        method: "POST",
-        body: JSON.stringify({
-          name: name,
-          email: email,
-          mobileNumber: mobileNumber,
-        }),
-      });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setName("");
-        setEmail("");
-        setMessage("User created successfully");
-      } else {
-        setMessage("Some error occured");
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
+const App = () => (
   
-  return(
-    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
     <Navbar />
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          placeholder="Name"
-          onChange={(e) => setName(e.target.value)}
-        /><br/>
-        <input
-          type="text"
-          value={email}
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="text"
-          value={mobileNumber}
-          placeholder="Mobile Number"
-          onChange={(e) => setMobileNumber(e.target.value)}
-        />
-
-        <button type="submit">Create</button>
-
-        <div className="message">{message ? <p>{message}</p> : null}</div>
-      </form>
-    </div>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/exercise/:id" element={<ExerciseDetail />} />
     </Routes>
     <Footer />
   </Box>
-  )
-};
+);
 
 export default App;

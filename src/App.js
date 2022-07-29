c3c68583a43609e8e832e82a6b1cb32064290698
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-07-29 13:53:47 +0300

    create/POST form

--- src/App.js
+++ src/App.js
@@ -8,15 +8,72 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-const App = () => (
-  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+const App = () => {
+    const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          mobileNumber: mobileNumber,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setName("");
+        setEmail("");
+        setMessage("User created successfully");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  return(
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
     <Navbar />
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        /><br/>
+        <input
+          type="text"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          value={mobileNumber}
+          placeholder="Mobile Number"
+          onChange={(e) => setMobileNumber(e.target.value)}
+        />
+
+        <button type="submit">Create</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
+    </div>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/exercise/:id" element={<ExerciseDetail />} />
     </Routes>
     <Footer />
   </Box>
-);
+  )
+};
 
 export default App;
